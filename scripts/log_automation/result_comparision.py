29d44547aa2a9347ae6643c27125cd7eed024342
Meri Aydinyan <meri@rapidsilicon.com>
2022-03-23 21:24:22 +0500

    Fixed csv analyzer script and regolden CI logs.

--- scripts/log_automation/result_comparision.py
+++ scripts/log_automation/result_comparision.py
@@ -9,16 +9,15 @@ parser.add_argument("file",
                     help="csv file for analyzing")
 args = parser.parse_args()
 df = pd.read_csv( args.file )
-data = pd.DataFrame(columns=["Benchmarks", "Average", "Minimum", "Maximum"])
-p = [i for i in df if i.startswith("PERCENTAGE Yosys")]
-percentage_list = list(df[p[0]])
+percentage_list = [i for i in df if ("PERCENTAGE" in i) and ("LUT" in i)]
 index = 2
+flag = True
 
-def add_min_benchmarks(data, min_val):
+def add_min_benchmarks(data, min_val, values):
     global index
     m = 0
     min_list = []
-    for i in percentage_list:
+    for i in values:
         if i == min_val:
             min_list.append(df['Benchmarks'][m])
         m += 1
@@ -27,11 +26,11 @@ def add_min_benchmarks(data, min_val):
         data.at[index, "Minimum"] = min_val  
         index += 1
 
-def add_max_benchmarks(data, max_val):
+def add_max_benchmarks(data, max_val, values):
     global index
     m = 0
     max_list = []
-    for i in percentage_list:
+    for i in values:
         if i == max_val:
             max_list.append(df['Benchmarks'][m])
         m += 1
@@ -41,35 +40,44 @@ def add_max_benchmarks(data, max_val):
         index += 1
 
 def flag_condition(min_val, average):
+    global flag
     if min_val >= 0 and average > 0:
         print("The status of QoR is ", colored("GREEN.", "white", "on_green"))
     elif min_val >= -5 and average > 0:
         print("The status of QoR is ", colored("BLUE.", "white", "on_blue"))
     elif min_val >= -5 and average <= 0:
-        print("The status of QoR is ", colored("YELLOW.", "black", "on_yellow"))
+        print("The status of QoR is Yellow", colored("YELLOW.", "white", "on_yellow"))
     elif min_val < -5 and average > 0:
         print("The status of QoR is ", colored("MAGENTA.", "white", "on_magenta"))
     elif min_val < -5 and average <= 0:
         print("The status of QoR is ", colored("RED.", "white", "on_red"))
-        sys.exit(1)
+        flag = False
+
 def check_status():
     for i in df:
         if "STATUS" in i:
             if "Fail" in list(df[i]): sys.exit("Some benchmarks have failed.")
 
 def main():
-     
-    min_val = min(percentage_list)
-    max_val = max(percentage_list) 
-    average = sum(list(df[p[0]])) / len(list(df[p[0]]))
-    data.at[1, "Average"] = average
-    add_min_benchmarks(data, min_val)
-    add_max_benchmarks(data, max_val)
-    blankIndex=[''] * len(data)
-    data.index=blankIndex
-    print(data.fillna('-'))
-    flag_condition(min_val, average)
+    for i in percentage_list:
+        data = pd.DataFrame(columns=["Benchmarks", "Average", "Minimum", "Maximum"])
+        values = list(df[i][: -4])
+        min_val = min(values)
+        max_val = max(values) 
+        average = sum(list(df[i][:-4])) / len(list(df[i][:-4]))
+        data.at[1, "Average"] = average
+        add_min_benchmarks(data, min_val, values)
+        add_max_benchmarks(data, max_val, values)
+        blankIndex=[''] * len(data)
+        data.index=blankIndex
+        print(i)
+        print(data.fillna('-'))
+        flag_condition(min_val, average)
     check_status()
+    if flag:
+        pass
+    else:
+        sys.exit(1)
 
 if __name__ == "__main__":
         main()
