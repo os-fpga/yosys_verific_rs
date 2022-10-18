dbd7615c62f60d2de7e6722d51bcc18693796eca
Bella Baghdasaryan <bella@rapidsilicon.com>
2022-10-18 17:10:44 +0500

    Added missing verilog files

---
+++ analyze/tests/gpio/rtl/prim_generic_buf.sv
@@ -0,0 +1,13 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+
+module prim_generic_buf (
+  input in_i,
+  output logic out_o
+);
+
+  assign out_o = in_i;
+
+endmodule
