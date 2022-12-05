56e0f416214bc677170379002de32b61a08e1707
Aram Kostanyan <aram@rapidsilicon.com>
2022-12-05 11:42:44 +0400

    Updated gcc version for CI.

--- .github/scripts/install_centos_dependencies_build.sh
+++ .github/scripts/install_centos_dependencies_build.sh
@@ -6,11 +6,11 @@ curl -C - -O https://cmake.org/files/v3.15/cmake-3.15.7-Linux-x86_64.tar.gz
 tar xzf cmake-3.15.7-Linux-x86_64.tar.gz
 ln -s $PWD/cmake-3.15.7-Linux-x86_64/bin/cmake /usr/bin/cmake
 yum install -y openssh-server openssh-clients
-yum install -y centos-release-scl
-yum install -y devtoolset-9
-yum install -y devtoolset-9-toolchain
-yum install -y devtoolset-9-gcc-c++
-scl enable devtoolset-9 bash
+yum install -y centos-release-scl-rh
+yum install -y devtoolset-11
+yum install -y devtoolset-11-toolchain
+yum install -y devtoolset-11-gcc-c++
+scl enable devtoolset-11 bash
 yum install -y tcl
 yum install -y make
 yum install -y flex
@@ -41,5 +41,5 @@ yum install -y openssl-devel
 
 
 ln -s $PWD/cmake-3.15.7-Linux-x86_64/bin/ctest /usr/bin/ctest
-echo 'QMAKE_CC=/opt/rh/devtoolset-9/root/usr/bin/gcc' >> $GITHUB_ENV
-echo 'QMAKE_CXX=/opt/rh/devtoolset-9/root/usr/bin/g++' >> $GITHUB_ENV
+echo 'QMAKE_CC=/opt/rh/devtoolset-11/root/usr/bin/gcc' >> $GITHUB_ENV
+echo 'QMAKE_CXX=/opt/rh/devtoolset-11/root/usr/bin/g++' >> $GITHUB_ENV
