aa1ace454fb8edb20e982b30dd901ee70f1dac11
Aram Kostanyan <aram@rapidsilicon.com>
2022-12-08 21:47:33 +0400

    CI test 2

--- .github/scripts/install_macos_dependencies_build.sh
+++ .github/scripts/install_macos_dependencies_build.sh
@@ -1,25 +1,27 @@
 set -x
 # Install required dependencies for Mac OS systems
-brew install bison \
-    flex \
-    gawk \
-    libffi \
-    pkg-config \
-    bash \
-    readline \
-    ninja \
-    wget \
-    coreutils \
-    openssl@3
+#brew install bison \
+#    flex \
+#    gawk \
+#    libffi \
+#    pkg-config \
+#    bash \
+#    readline \
+#    ninja \
+#    wget \
+#    coreutils \
+#    openssl@3
 
-ver=`/usr/local/opt/openssl@3/bin/openssl version`
-num_ver=${ver:8:5}
+find ./ -type d -name lib | xargs ls -la
 
-ln -s $(brew --prefix openssl)/include/openssl /usr/local/include
-ln -s $(brew --prefix openssl)/lib/libssl.${num_ver}.dylib /usr/local/lib
-ln -s $(brew --prefix openssl)/lib/libcrypto.${num_ver}.dylib /usr/local/lib
-ln -s $(brew --prefix openssl)/lib/libssl.${num_ver}.a /usr/local/lib
-ln -s $(brew --prefix openssl)/lib/libcrypto.${num_ver}.a /usr/local/lib
-
-ls -la /usr/local/include
-ls -la /usr/local/lib
+#ver=`/usr/local/opt/openssl@3/bin/openssl version`
+#num_ver=${ver:8:5}
+#
+#ln -s $(brew --prefix openssl)/include/openssl /usr/local/include
+#ln -s $(brew --prefix openssl)/lib/libssl.${num_ver}.dylib /usr/local/lib
+#ln -s $(brew --prefix openssl)/lib/libcrypto.${num_ver}.dylib /usr/local/lib
+#ln -s $(brew --prefix openssl)/lib/libssl.${num_ver}.a /usr/local/lib
+#ln -s $(brew --prefix openssl)/lib/libcrypto.${num_ver}.a /usr/local/lib
+#
+#ls -la /usr/local/include
+#ls -la /usr/local/lib
