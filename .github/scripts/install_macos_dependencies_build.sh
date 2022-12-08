369e993dcf6c1a4f8b1c7c8485b99b847f650a3e
Aram Kostanyan <aram@rapidsilicon.com>
2022-12-08 11:35:13 +0400

    Attempt N+4

--- .github/scripts/install_macos_dependencies_build.sh
+++ .github/scripts/install_macos_dependencies_build.sh
@@ -10,3 +10,9 @@ brew install bison \
     wget \
     coreutils \
     openssl
+
+ln -s $(brew --prefix openssl)/include/openssl /usr/local/include
+ln -s $(brew --prefix openssl)/lib/libssl* /usr/local/lib
+ln -s $(brew --prefix openssl)/lib/libcrypto* /usr/local/lib
+
+ls -la /usr/local/lib
