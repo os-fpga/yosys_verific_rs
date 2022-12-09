bf93090540f0dd9da1f4e8b44ccf90e99bbfc507
Aram Kostanyan <aram@rapidsilicon.com>
2022-12-09 15:51:59 +0400

    Hardcoded link paths.

--- .github/scripts/install_macos_dependencies_build.sh
+++ .github/scripts/install_macos_dependencies_build.sh
@@ -15,11 +15,8 @@ brew install bison \
 find /usr/local/ -name *libssl*
 find /usr/local/ -name *libcrypto*
 
-ver=`/usr/local/opt/openssl@1.1/bin/openssl version`
-num_ver=${ver:8:5}
-
 ln -s $(brew --prefix openssl@1.1)/include/openssl /usr/local/include
-ln -s $(brew --prefix openssl@1.1)/lib/libssl.${num_ver}.dylib /usr/local/lib
-ln -s $(brew --prefix openssl@1.1)/lib/libcrypto.${num_ver}.dylib /usr/local/lib
-ln -s $(brew --prefix openssl@1.1)/lib/libssl.a /usr/local/lib
-ln -s $(brew --prefix openssl@1.1)/lib/libcrypto.a /usr/local/lib
+ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libssl.dylib /usr/local/lib
+ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libssl.a /usr/local/lib
+ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libcrypto.a /usr/local/lib
+ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libcrypto.dylib /usr/local/lib
