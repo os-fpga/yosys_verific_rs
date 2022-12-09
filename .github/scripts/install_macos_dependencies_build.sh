42223dbd85984fdb711cb6f69cecc34adc392230
Aram Kostanyan <aram@rapidsilicon.com>
2022-12-09 22:37:18 +0400

    Attempt to add path in CMake.

--- .github/scripts/install_macos_dependencies_build.sh
+++ .github/scripts/install_macos_dependencies_build.sh
@@ -12,13 +12,13 @@ brew install bison \
     coreutils \
     openssl@1.1
 
-find /usr/local/ -name *libssl*
-find /usr/local/ -name *libcrypto*
-
-ln -s $(brew --prefix openssl@1.1)/include/openssl /usr/local/include
-ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libssl.dylib /usr/local/lib
-ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libssl.a /usr/local/lib
-ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libcrypto.a /usr/local/lib
-ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libcrypto.dylib /usr/local/lib
-
-ls -la /usr/local//Cellar/openssl@1.1/1.1.1s/lib
+#find /usr/local/ -name *libssl*
+#find /usr/local/ -name *libcrypto*
+#
+#ln -s $(brew --prefix openssl@1.1)/include/openssl /usr/local/include
+#ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libssl.dylib /usr/local/lib
+#ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libssl.a /usr/local/lib
+#ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libcrypto.a /usr/local/lib
+#ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libcrypto.dylib /usr/local/lib
+#
+#ls -la /usr/local//Cellar/openssl@1.1/1.1.1s/lib
