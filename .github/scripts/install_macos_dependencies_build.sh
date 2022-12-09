8db41f073fb5079ee0ad2edd94f07237fb0caf9a
Aram Kostanyan <aram@rapidsilicon.com>
2022-12-09 17:06:27 +0400

    Debug attempt.

--- .github/scripts/install_macos_dependencies_build.sh
+++ .github/scripts/install_macos_dependencies_build.sh
@@ -16,12 +16,7 @@ find /usr/local/ -name *libssl*
 find /usr/local/ -name *libcrypto*
 
 ln -s $(brew --prefix openssl@1.1)/include/openssl /usr/local/include
-ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libssl.dylib /usr/local/lib/libssl.dylib
-ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libssl.a /usr/local/lib/libssl.a
-ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libcrypto.a /usr/local/lib/libcrypto.a
-ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libcrypto.dylib /usr/local/lib/libcrypto.dylib
-sudo ln -s $(brew --prefix openssl@1.1)/include/openssl /usr/include
-sudo ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libssl.dylib /usr/lib/libssl.dylib
-sudo ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libssl.a /usr/lib/libssl.a
-sudo ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libcrypto.a /usr/lib/libcrypto.a
-sudo ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libcrypto.dylib /usr/lib/libcrypto.dylib
+ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libssl.dylib /usr/local/lib
+ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libssl.a /usr/local/lib
+ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libcrypto.a /usr/local/lib
+ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libcrypto.dylib /usr/local/lib
