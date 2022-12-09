20f422572d58816c2c0fa16fcb33b619d4dc6405
Aram Kostanyan <aram@rapidsilicon.com>
2022-12-09 16:44:52 +0400

    Attempt NNNNNN.

--- .github/scripts/install_macos_dependencies_build.sh
+++ .github/scripts/install_macos_dependencies_build.sh
@@ -20,3 +20,8 @@ ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libssl.dylib /usr/local/lib/libs
 ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libssl.a /usr/local/lib/libssl.a
 ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libcrypto.a /usr/local/lib/libcrypto.a
 ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libcrypto.dylib /usr/local/lib/libcrypto.dylib
+ln -s $(brew --prefix openssl@1.1)/include/openssl /usr/include
+ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libssl.dylib /usr/lib/libssl.dylib
+ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libssl.a /usr/lib/libssl.a
+ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libcrypto.a /usr/lib/libcrypto.a
+ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libcrypto.dylib /usr/lib/libcrypto.dylib
