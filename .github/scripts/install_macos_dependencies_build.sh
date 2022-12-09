1cabdffe335c03bf298ddd878d186a212a5db57f
Aram Kostanyan <aram@rapidsilicon.com>
2022-12-09 15:11:46 +0400

    Enable the steps.

--- .github/scripts/install_macos_dependencies_build.sh
+++ .github/scripts/install_macos_dependencies_build.sh
@@ -15,22 +15,11 @@ brew install bison \
 find /usr/local/ -name *libssl*
 find /usr/local/ -name *libcrypto*
 
-#ver=`/usr/local/opt/openssl@1.1/bin/openssl version`
-#num_ver=${ver:8:5}
-#
-#ln -s $(brew --prefix openssl@1.1)/include/openssl /usr/local/include
-#ln -s $(brew --prefix openssl@1.1)/lib/libssl.${num_ver}.dylib /usr/local/lib
-#ln -s $(brew --prefix openssl@1.1)/lib/libcrypto.${num_ver}.dylib /usr/local/lib
-#ln -s $(brew --prefix openssl@1.1)/lib/libssl.${num_ver}.a /usr/local/lib
-#ln -s $(brew --prefix openssl@1.1)/lib/libcrypto.${num_ver}.a /usr/local/lib
+ver=`/usr/local/opt/openssl@1.1/bin/openssl version`
+num_ver=${ver:8:5}
 
-#ls -la -R $(brew --prefix openssl)
-#ls -la -R /usr/local/Cellar
-#ls -la -R /usr/local/opt
-#ls -la -R /usr/opt
-#ls -la -R /usr/Cellar
-#
-#ls -la -R /usr/local/include
-#ls -la -R /usr/local/lib
-#
-#ls -la -R /usr/
+ln -s $(brew --prefix openssl@1.1)/include/openssl /usr/local/include
+ln -s $(brew --prefix openssl@1.1)/lib/libssl.${num_ver}.dylib /usr/local/lib
+ln -s $(brew --prefix openssl@1.1)/lib/libcrypto.${num_ver}.dylib /usr/local/lib
+ln -s $(brew --prefix openssl@1.1)/lib/libssl.a /usr/local/lib
+ln -s $(brew --prefix openssl@1.1)/lib/libcrypto.a /usr/local/lib
