44e0d16560cfb45ab0f2720a758d4dba7c0b61db
Aram Kostanyan <aram@rapidsilicon.com>
2022-12-09 14:18:12 +0400

    Test NNNNNNNN

--- .github/scripts/install_macos_dependencies_build.sh
+++ .github/scripts/install_macos_dependencies_build.sh
@@ -12,24 +12,25 @@ brew install bison \
     coreutils \
     openssl@1.1
 
-# sudo find / -type d -name lib | xargs ls -la
+find /usr/local/ -name *libssl*
+find /usr/local/ -name *libcrypto*
 
-ver=`/usr/local/opt/openssl@1.1/bin/openssl version`
-num_ver=${ver:8:5}
+#ver=`/usr/local/opt/openssl@1.1/bin/openssl version`
+#num_ver=${ver:8:5}
+#
+#ln -s $(brew --prefix openssl@1.1)/include/openssl /usr/local/include
+#ln -s $(brew --prefix openssl@1.1)/lib/libssl.${num_ver}.dylib /usr/local/lib
+#ln -s $(brew --prefix openssl@1.1)/lib/libcrypto.${num_ver}.dylib /usr/local/lib
+#ln -s $(brew --prefix openssl@1.1)/lib/libssl.${num_ver}.a /usr/local/lib
+#ln -s $(brew --prefix openssl@1.1)/lib/libcrypto.${num_ver}.a /usr/local/lib
 
-ln -s $(brew --prefix openssl@1.1)/include/openssl /usr/local/include
-ln -s $(brew --prefix openssl@1.1)/lib/libssl.${num_ver}.dylib /usr/local/lib
-ln -s $(brew --prefix openssl@1.1)/lib/libcrypto.${num_ver}.dylib /usr/local/lib
-ln -s $(brew --prefix openssl@1.1)/lib/libssl.${num_ver}.a /usr/local/lib
-ln -s $(brew --prefix openssl@1.1)/lib/libcrypto.${num_ver}.a /usr/local/lib
-
-ls -la -R $(brew --prefix openssl)
-ls -la -R /usr/local/Cellar
-ls -la -R /usr/local/opt
-ls -la -R /usr/opt
-ls -la -R /usr/Cellar
-
-ls -la -R /usr/local/include
-ls -la -R /usr/local/lib
-
-ls -la -R /usr/
+#ls -la -R $(brew --prefix openssl)
+#ls -la -R /usr/local/Cellar
+#ls -la -R /usr/local/opt
+#ls -la -R /usr/opt
+#ls -la -R /usr/Cellar
+#
+#ls -la -R /usr/local/include
+#ls -la -R /usr/local/lib
+#
+#ls -la -R /usr/
