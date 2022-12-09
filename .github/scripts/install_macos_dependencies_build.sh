409879a9cc8e8408c1ac0a54b8e7d1f90ce48786
Aram Kostanyan <aram@rapidsilicon.com>
2022-12-09 17:30:24 +0400

    Debug 1234.

--- .github/scripts/install_macos_dependencies_build.sh
+++ .github/scripts/install_macos_dependencies_build.sh
@@ -20,3 +20,5 @@ ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libssl.dylib /usr/local/lib
 ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libssl.a /usr/local/lib
 ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libcrypto.a /usr/local/lib
 ln -s /usr/local//Cellar/openssl@1.1/1.1.1s/lib/libcrypto.dylib /usr/local/lib
+
+ls -la /usr/local//Cellar/openssl@1.1/1.1.1s/lib
