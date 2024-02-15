abea6630573a32827e6ca74c92f03e1ce11bde8b
NadeemYaseen <nadeem.yaseen@rapidsilicon.com>
2024-02-15 20:33:45 +0500

    updated cmake version for centos 7

--- .github/scripts/install_centos_dependencies_build.sh
+++ .github/scripts/install_centos_dependencies_build.sh
@@ -2,9 +2,9 @@
 yum update -y
 yum group install -y "Development Tools"
 yum install -y epel-release
-curl -C - -O https://cmake.org/files/v3.15/cmake-3.15.7-Linux-x86_64.tar.gz
-tar xzf cmake-3.15.7-Linux-x86_64.tar.gz
-ln -s $PWD/cmake-3.15.7-Linux-x86_64/bin/cmake /usr/bin/cmake
+curl -C - -O https://cmake.org/files/v3.28/cmake-3.28.0-linux-x86_64.tar.gz
+tar xzf cmake-3.28.0-linux-x86_64.tar.gz
+ln -s $PWD/cmake-3.28.0-linux-x86_64/bin/cmake /usr/bin/cmake
 yum install -y openssh-server openssh-clients
 yum install -y centos-release-scl-rh
 yum install -y devtoolset-11
@@ -44,6 +44,6 @@ pip3 install orderedmultidict
 pip3 install psutil
 
 
-ln -s $PWD/cmake-3.15.7-Linux-x86_64/bin/ctest /usr/bin/ctest
+ln -s $PWD/cmake-3.28.0-linux-x86_64/bin/ctest /usr/bin/ctest
 echo 'QMAKE_CC=/opt/rh/devtoolset-11/root/usr/bin/gcc' >> $GITHUB_ENV
 echo 'QMAKE_CXX=/opt/rh/devtoolset-11/root/usr/bin/g++' >> $GITHUB_ENV
